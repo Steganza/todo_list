d9635bfaaa9752e1d8931c3ee34e32d7a6b3c915
Steganza <stefanbotha1@gmail.com>
2024-08-22 18:01:56 -0400

    added list view

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -12,10 +12,21 @@ const TodoList = () => {
             setHeadingInput('');
         }
     };
+    const handleAddList = (index) => {
+        if (listInputs[index] && listInputs[index].trim() !== ''){
+            const newTodos = [...todos];
+            newTodos[index].lists.push(listInputs[index]);
+            setTodos(newTodos);
+            setListInputs({ ...listInputs, [index]: ''});
+        }
+    };
+    consthandleListInputChange = (index, value) => {
+        setListInputs({ ...listInputs, [index]: value});
+;    }
 
   return (
     <>
-      <div className="todo-container">
+     <div className="todo-container">
         <h1 className="title">My Todo List</h1>
         <div className="input-container">
           <input
@@ -34,6 +45,23 @@ const TodoList = () => {
                 <div className="heading_todo">
                     <h3>{todo.heading}</h3> {/* Display the heading here */}
                     <button className="delete-button-heading">Delete Heading</button>
+                    <ul>
+                    {todo.lists.map((list, listIndex) => (
+                        <li key={listIndex} className='todo_inside_list'>
+                            <p>{list}</p>
+                        </li>
+                    ))}
+                    </ul>
+
+                    <div className='add_list'>
+                        <input
+                        type="text"
+                        className="list-input"
+                        placeHolder="Add List"
+                        value={listInputs[index] || ''}
+                        onChange={(e) => handleListInputChange(index, e.target.value)}/>
+                        <button className="add-list-button" onClick={() => handleAddList(index)}>Add list</button>
+                    </div>
                 </div>
             </div>
         ))}
